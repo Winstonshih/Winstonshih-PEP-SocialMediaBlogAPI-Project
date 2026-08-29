877e23bb93146c77617e141036a78423dbe09ef0
Winstonshih <Winstonshih@github.com>
2026-08-29 20:59:19 +0000

    Test run: 9/23 passed, 14 failed, 0 skipped - 2026-08-29T20:59:19.766Z

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -40,24 +40,4 @@ public class MessageDAO {
     }
     return null;
  }
- public List<Message> getAllMessages()
- {
-    Connection connection = ConnectionUtil.getConnection();
-        List<Message> messages = new ArrayList<>();
-        try {
-            String sql = "select * from message;";
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            ResultSet rs = preparedStatement.executeQuery();
-            while(rs.next()){
-                Message m = new Message(rs.getInt("message_id"),
-                        rs.getInt("posted_by"),
-                        rs.getString("meessage_text"),
-                        rs.getLong("time_posted_epoch"));
-                messages.add(m);
-            }
-        }catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
-        return messages;
- }
 }
