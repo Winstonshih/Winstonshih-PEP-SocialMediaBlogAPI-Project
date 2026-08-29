2d9f5b1a3f43b956b59fa2302d17a712f2b8cfaf
Winstonshih <Winstonshih@github.com>
2026-08-29 22:48:08 +0000

    Test run: 16/23 passed, 7 failed, 0 skipped - 2026-08-29T22:48:08.051Z

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -34,6 +34,6 @@ public class MessageService {
         return null;
     }
     messageDAO.getMessageById(id).setMessage_text(m.getMessage_text());
-    return messageDAO.updateMessage(id, messageDAO.getMessageById(id));
+    return this.messageDAO.updateMessage(id, messageDAO.getMessageById(id));
   }
 }
